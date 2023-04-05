a21cc12fbece17630b1ea516908bd29bb05747b0
bpinedad <bpinedad96@gmail.com>
2023-04-05 01:05:14 -0400

    Init

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public float speed = 0.5f;
+	public float maxY = 10f;
+	private int direction = 1;
+	private float initialY;
+
+
+	// Use this for initialization
+	void Start () {
+		initialY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float currentY = transform.position.y;
+
+		//Move up while direction is up and below max Y
+		if (direction == 1 && currentY > maxY) {
+			direction = -1;
+		} else if (direction == -1 && currentY < initialY) {
+			direction = 1;
+		}
+
+		transform.Translate(new Vector3(0, direction * speed * Time.deltaTime, 0), Space.World);
+	}
+}
---
+++ Assets/Scripts/WinLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WinLevel : MonoBehaviour {
+
+	public Text canvas;
+
+	void OnTriggerEnter(Collider other) {
+		canvas.enabled = true;
+		GetComponent<AudioSource>().Play();
+		GetComponent<MeshRenderer>().enabled = false;
+		GetComponent<SphereCollider>().enabled = false;
+	}
+}
